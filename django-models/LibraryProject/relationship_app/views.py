ea4315e75ba503fc30b00b5b9c6a21cff72f335c
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-11-06 15:53:10 +0100

    implementing a class-based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 from .models import Book
+from .models import Library
 
 # Create your views here.
 def list_books(request):
@@ -7,3 +9,17 @@ def list_books(request):
     books = Book.objects.all()
     context = {'list_books': books}
     return render(request, 'relationship_app/list_books.html', context)
+
+class BookDetailView(DetailView):
+    """A call_based view for displaying details of a specific library. """
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+    
+    def get_context_data(self, **kwargs):
+        """Injects additional context data specific to the book."""
+        context = super().get_context_data(**kwargs)
+        context['book'] = self.object.books.all()
+        return context
+    
+    
