f0511f516dda8e454687fdf17a12b16eaa618248
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-11-06 14:32:28 +0100

    implement function based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
+def book_list(request):
+    """Retrieves all books and renders a tempate displaying the list."""
+    books = Book.objects.all()
+    output = "List of Books:\n"
+    for book in books:
+        output += f"-{book.title} by {book.author}"
+        
+    return HttpResponse(output, content_type= "text/plain")
