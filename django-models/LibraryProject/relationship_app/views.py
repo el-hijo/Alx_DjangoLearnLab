ec2a1b0ff425f45ccb6548d4788a12cf7d0e2e36
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-11-07 12:19:28 +0100

    class based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 
 # Function-based view
 def list_books(request):
     """Lists all books stored in the database"""
     books = Book.objects.all()
-    #output = "\n".join([f"{book.title} by {book.author}" for book in books])
-    #return HttpResponse(output, content_type= "text/plain")
     context = {'list_book': books}
     return render(request, 'relationship_app/list_books.html', context)
 
@@ -18,7 +17,7 @@ def list_books(request):
 class BookDetailView(DetailView):
     """Displays details for a specific library and its books"""
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
