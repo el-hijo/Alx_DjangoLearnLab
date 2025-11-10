55ac46fad9a6c4ecfdaaf3f346e5abd50bfc34f6
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-11-10 14:59:42 +0100

    intial commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth import login, logout
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
+
 
 
 # Function-based view
@@ -68,3 +71,23 @@ def user_logout(request):
     messages.success(request, "Logged out successfully!")
     return redirect('login')
 
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
