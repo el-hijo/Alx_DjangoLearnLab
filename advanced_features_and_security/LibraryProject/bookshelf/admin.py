bf909011105bfed6bc0fcc5df61bc85041a56275
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-11-13 11:57:44 +0100

    commit initial

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 # Register your models here.
 from .models import Book
