db0981335765f8c1667116c0303b02293babcf73
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-11-13 11:01:26 +0100

    fourth commit

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,7 @@
+
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
 from .models import Book
@@ -10,3 +13,13 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_year',)
     
 admin.site.register(Book, BookAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    # Optional: if you have extra fields, add them here
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {'fields': ('profile_photo',)}),  # example
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Extra Info', {'fields': ('profile_photo',)}),
+    )
