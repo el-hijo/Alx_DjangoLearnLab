3d80956bdd6467951f8ac44e80c271ce66b21809
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-11-21 11:28:50 +0100

    defined permission classes

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
 # Create your views here.
@@ -8,7 +9,9 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
