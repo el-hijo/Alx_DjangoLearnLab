6bfb199df334e4b499c7e9b80156e1790f412b9f
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-11-26 15:42:21 +0100

    created views

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -2,12 +2,22 @@ from rest_framework import serializers
 from .models import Author, Book
 from datetime import date
 
+
+"""
+    Serializer for the Book model.
+    Serializes all fields and adds custom validation
+    to ensure publication_year is not in the future.
+    """
 class BookSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Book
         fields = "__all__"
     
+    """
+        Field-level validation for publication_year.
+        Ensures the year is not in the future.
+        """
     def validate_publication_year(self, value):
         current_year = date.today().year
         if value > current_year:
@@ -16,7 +26,10 @@ class BookSerializer(serializers.ModelSerializer):
  
         return value
     
-    
+"""
+    Serializer for the Author model.
+    Includes the author's name and a nested list of books.
+    """    
 class AuthorSerializer(serializers.ModelSerializer):
     books = BookSerializer(many=True, read_only=True)
     
