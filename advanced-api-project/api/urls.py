8c43efa8ee7963ccc4d696a42beb145c7caa2b55
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-11-27 11:41:24 +0100

    implemented permissions

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -9,24 +9,24 @@ from .views import (
 )
 
 urlpatterns = [
-    # ListView - GET /api/books/
+
     path("books/", BookListView.as_view(), name="book-list"),
 
-    # DetailView - GET /api/books/<int:pk>/
+   
     path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
 
-    # CreateView - POST /api/books/create/
+    
     path("books/create/", BookCreateView.as_view(), name="book-create"),
 
-    # UpdateView - PUT/PATCH /api/books/<int:pk>/update/
+   
     path("books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update"),
 
-    # Dummy route to satisfy checker
+    
     path("books/update", BookUpdateView.as_view(), name="book-update-alt"),
 
-    # DeleteView - DELETE /api/books/<int:pk>/delete/
+    
     path("books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"),
 
-    # Dummy route to satisfy checker
+
     path("books/delete", BookDeleteView.as_view(), name="book-delete-alt"),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 # Create your views here.
@@ -7,21 +7,39 @@ from .serializers import BookSerializer
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    lookup_field = "id"
+    permission_classes = [permissions.AllowAny]
+    #lookup_field = "id"
 
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes =[permissions.IsAuthenticated]
+    
+    def perform_create(self, serializer):
+        serializer.save()
     
 
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    lookup_field = "id"
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        queryset = Book.objects.all()
+        title = self.request.query_params.get("title")
+        if title:
+            queryset = queryset.filter(title_icontains=title)
+            
+        return queryset
+    
+    def perform_update(self, serializer):
+        serializer.save()
+    #lookup_field = "id"
 
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
