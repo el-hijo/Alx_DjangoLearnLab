2272ff82c6678ded497691536c9df212632795c8
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-11-27 13:36:30 +0100

    implemented search functionality

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,9 +3,11 @@ from django_filters import rest_framework
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 from .models import Book
 from .serializers import BookSerializer
+
 
 
 
@@ -18,6 +20,8 @@ class BookListView(generics.ListAPIView):
     
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["title","author","publication_year"]
+    search_fields = ["title", "author"]
+    
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
