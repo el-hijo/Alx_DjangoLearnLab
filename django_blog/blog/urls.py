090c654f975cfe16698d26260019691bb5d17f28
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-12-04 11:10:18 +0100

    final commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/',views.PostDetailView.as_view(), name= 'post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
 
