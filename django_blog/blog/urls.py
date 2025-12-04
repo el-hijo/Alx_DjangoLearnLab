d254294c9bfd4d4a11bed5942e20e884f6cec5e9
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-12-04 11:00:32 +0100

    made corrections to CRUD implementation

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import CustomLoginView, CustomLogoutView, register, profile
+from . import views
 
 app_name = "blog"
 
@@ -8,4 +9,9 @@ urlpatterns = [
     path("logout/", CustomLogoutView.as_view(), name="logout"),
     path("register/", register, name="register"),
     path("profile/", profile, name="profile"),
+    path('', views.PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,8 +3,12 @@ from django.urls import reverse_lazy
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
-
 from .forms import CustomUserCreationForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+
 
 class CustomLoginView(LoginView):
     template_name = "blog/login.html"
@@ -30,3 +34,43 @@ def profile(request):
     # simple profile view that renders current user information
     return render(request, "blog/profile.html", {"user": request.user})
 
+
+
+class ListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'   
+    context_object_name = 'posts'
+    paginate_by = 10                         
+
+class DetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+class CreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = CustomUserCreationForm                # or use `fields = ['title', 'content']`
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        # set the author automatically to the logged-in user
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class UpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = CustomUserCreationForm                  # or fields = [...]
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+class tDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:post-list')  # adjust namespace/name as you choose
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
