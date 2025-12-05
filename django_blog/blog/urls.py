2c07c16ce714444ad03b26e84704cbdac25f39c5
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-12-05 15:39:00 +0100

    correct urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,22 +1,34 @@
 from django.urls import path
-from .views import CustomLoginView, CustomLogoutView, register, profile, CommentCreateView,CommentUpdateView, CommentDeleteView
 from . import views
-
-app_name = "blog"
+from .views import (
+    CustomLoginView,
+    CustomLogoutView,
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    CommentCreateView,
+    CommentUpdateView,
+    CommentDeleteView,
+)
 
 urlpatterns = [
     path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", CustomLogoutView.as_view(), name="logout"),
-    path("register/", register, name="register"),
-    path("profile/", profile, name="profile"),
-    path('', views.PostListView.as_view(), name='post-list'),
-    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update/',views.PostDetailView.as_view(), name= 'post-update'),
-    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
-    path("post/<int:pk>/comment/new/", CommentCreateView.as_view(), name="comment-create"),
+    path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile"),
+
+    
+    path("", PostListView.as_view(), name="post-list"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+
+    
+    path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="comment-create"),
     path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
 ]
-
-
+    
