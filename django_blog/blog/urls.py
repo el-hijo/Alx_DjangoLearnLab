dd6a13e675141c450db06755560189d9c95f2b53
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-12-05 15:11:48 +0100

    changed 'edit' to 'update'

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('post/<int:pk>/update/',views.PostDetailView.as_view(), name= 'post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
     path("post/<int:pk>/comment/new/", CommentCreateView.as_view(), name="comment-create"),
-    path("comment/<int:pk>/edit/", CommentUpdateView.as_view(), name="comment-edit"),
+    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
 ]
 
