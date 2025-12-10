5d8409ae27b40b48b512ba9221661a86f39fbb70
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-12-10 12:34:29 +0100

    created views for CRUD operations

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.exceptions import PermissionDenied
+
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
 # Create your views here.
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+    
+        post_id = self.request.data.get("post")
+
+        if not post_id:
+            raise PermissionDenied("Post ID is required to create a comment.")
+
+        serializer.save(author=self.request.user,post_id=post_id)
