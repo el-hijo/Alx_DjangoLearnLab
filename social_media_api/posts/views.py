c56ff94b025bb1382cb839cd049ce0fcec33b1ed
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-12-11 08:41:03 +0100

    created API Endpoints for managing follows

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.exceptions import PermissionDenied
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import User
 from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+from .serializers import PostSerializer, CommentSerializer, UserSerializer
 
 from rest_framework import filters
 # Create your views here.
@@ -40,3 +42,28 @@ class CommentViewSet(viewsets.ModelViewSet):
             raise PermissionDenied("Post ID is required to create a comment.")
 
         serializer.save(author=self.request.user,post_id=post_id)
+
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    @action(detail=True, methods=["post"], permission_classes=[permissions.IsAuthenticated])
+    def follow(self, request, pk=None):
+        target_user = self.get_object()
+
+        if target_user == request.user:
+            return Response({"detail": "You cannot follow yourself."},status=status.HTTP_400_BAD_REQUEST)
+        request.user.follow(target_user)
+        return Response({"detail": f"You are now following {target_user.username}."},status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=["post"], permission_classes=[permissions.IsAuthenticated])
+    def unfollow(self, request, pk=None):
+        target_user = self.get_object()
+
+        if not request.user.is_following(target_user):
+            return Response({"detail": "You are not following this user."},status=status.HTTP_400_BAD_REQUEST)
+        request.user.unfollow(target_user)
+        return Response({"detail": f"You have unfollowed {target_user.username}."},status=status.HTTP_200_OK)
