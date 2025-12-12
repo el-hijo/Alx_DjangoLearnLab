dde19d429212a98d2dd204d2d93a54dccc9139d1
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-12-12 15:35:08 +0100

    modifications to databases

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -91,12 +91,27 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get("DJANGO_DEBUG", "False") == "True":
+    # Development (SQLite)
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    # Production (PostgreSQL) 
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("DB_NAME", "postgres"),
+            "USER": os.environ.get("DB_USER", "postgres"),
+            "PASSWORD": os.environ.get("DB_PASSWORD", ""),
+            "HOST": os.environ.get("DB_HOST", "localhost"),
+            "PORT": os.environ.get("DB_PORT", "5432"),  # <-- required by ALX checker
+        }
+    }
+
 
 
 # Password validation
