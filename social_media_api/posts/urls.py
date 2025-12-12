3528dd7661c9712041d35d56c89e12c86a1a55ed
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-12-12 12:35:46 +0100

    created url patterns for likes & notifications

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, UserFeedView
+from .views import (PostViewSet, CommentViewSet, UserFeedView,LikePostView,UnlikePostView)
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
@@ -9,4 +9,8 @@ router.register(r'comments', CommentViewSet, basename='comment')
 urlpatterns = [
     path('', include(router.urls)),
     path("feed/", UserFeedView.as_view(), name="user-feed"),
+    path("posts/<int:pk>/like/", LikePostView.as_view(), name="like-post"),
+    path("posts/<int:pk>/unlike/", UnlikePostView.as_view(), name="unlike-post"),
 ]
+
+urlpatterns += router.urls
